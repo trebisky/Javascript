0debae10adc21f9c6c897b22c2946c0e9e922f3d
Tom Trebisky <tom@mmto.org>
2020-04-14 16:23:49 -0700

    Add a first project

---
+++ node_1_hello/hello.js
@@ -0,0 +1,6 @@
+/* The idea here is to write the simplest possible
+ * "node" program that prints hello world.
+ * This would be one line except for this wordy comment
+ * Run this via: node hello.js
+ */
+console.log ( "Hello world" );
