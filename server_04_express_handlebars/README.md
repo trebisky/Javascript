df74b9060f3f5b8b7d18b389e4bbf09e2ef2925d
Tom Trebisky <tom@mmto.org>
2020-04-24 18:52:08 -0700

    Handlebars demo

---
+++ server_04_express_handlebars/README.md
@@ -0,0 +1,10 @@
+# Handlebars
+
+This was a LOT of work.
+Getting handlebars set up is a pain, but once you get it going,
+it is a worthwhile thing.  In this example, I have kept the
+actual handlebars setup basic and tried to (mostly) just take
+all the defaults (other than changing the template file extension
+to ".hbs").
+
+All the interesting notes are in the comments in the info.js file.
---
+++ server_04_express_handlebars/info.js
@@ -0,0 +1,154 @@
+/* Tom Trebisky  4-24-2020
+ *
+ * This builds on server_03_info
+ * I don't intend to do anything new other than
+ * to pull in handlebars and use it to generate
+ * the response for the "/info" route.
+ * Having an html template in "views" should
+ * be a nice improvement over crafting the html
+ * using a bunch of res.write() calls.
+ *
+ * This is true in principle, but getting set up to
+ *  use handlebars is somewhat of a pain in the ass.
+ *
+ * The main problem is that the documentation for
+ * express-handlebars is mediocre at best, not to
+ * mention confusing.
+ *
+ * When you call res.render('info') you need to
+ * have two files in place:
+ *
+ *   views/info.hbs
+ *   views/layout/main.hbs
+ *
+ * main.hbs is a wrapper for all pages!
+ * info.hbs gets injected into {{body}}
+ *   in main.hbs
+ */
+
+var express = require('express');
+const port = 8080;
+
+var hb = require('express-handlebars');
+
+/* optional monkey business */
+var child_process = require('child_process');
+
+/* This gets called when the server is successfully launched.
+ */
+function listen_callback ()
+{
+    console.log('listening on port ' + port);
+}
+
+/* I apologize for this, but I wanted to play around with
+ * running system commands from javascript (on the server)
+ * and then letting handlebars inject their output into
+ * the displayed page.
+ *
+ * Multi line system commands like ls -l or ps -alx just
+ * make a mess since all the lines run together.
+ * We would have to add a bunch of <br> to make it look right.
+ * It doesn't matter since we are just horsing around.
+ */
+function get_command ()
+{
+    var cmd = "pwd";
+    return child_process.execSync ( cmd );
+}
+
+/* The ps command works now, but is really really long.
+ * And we get a Buffer and not a String.
+ */
+function get_command_lines ()
+{
+    // var cmd = "ps -alx";
+    var cmd = "ls -l";
+    var stuff = child_process.execSync ( cmd );
+    if ( typeof ( stuff ) == "string" ) {
+	return stuff.split ( "\n" );
+    } else {
+	return stuff.toString().split ( "\n" );
+    }
+}
+
+function get_command_multi ()
+{
+    var lines = get_command_lines();
+    var rv = "";
+
+    lines.forEach ( function (line) {
+	rv += "<br>" + line;
+    });
+    return rv;
+}
+
+/* This is really what Handlebars is all about.
+ * We can inject values into templates.
+ * They may be pages, layouts, or partials.
+ * There are also helpers, which allow logic inside
+ * of the templates (which live in the "views" directory).
+ */
+function info_handler ( req, res )
+{
+    var date = new Date();
+    var date_str = date.toLocaleString();
+    var sys = get_command();
+    var sys2 = get_command_multi();
+
+    return res.render('info',
+	{ date: date_str,
+	  system: sys,
+	  system2: sys2 }
+    );
+}
+
+/* I centralize all the handlebars setup rubbish here
+ * so that I have a central place to complain about it.
+ *
+ * There are lots of tutorials, most of which are confusing.
+ * The best source, amazingly, is by the author at:
+ *   https://www.npmjs.com/package/express-handlebars
+ *
+ * I tried to change the file extent from "handlebars"
+ *   to "hbs". It took a while to learn that the new
+ *   string needs to go in 3 different places!
+ * I use a variable to make it clear where these are.
+ * Note that you can include the "dot" or not, as you see fit.
+ */
+
+// const hb_ext = '.handlebars';
+const hb_ext = 'hbs';
+
+function hb_setup ( app )
+{
+    app.set ( 'view engine', hb_ext );
+    app.engine ( hb_ext, hb ({extname: hb_ext}) );
+}
+
+function run_server ()
+{
+    var app = express();
+
+    app.use( express.static('public') );
+
+    hb_setup ( app );
+
+    app.get ( "/info", info_handler );
+
+    // app.listen ( port, () => console.log('listening on port ' + port));
+    app.listen ( port, listen_callback );
+}
+
+/* Note that "app.use" installs middleware, in this case the
+* "static" processing module.
+*
+* Also note the use of the "arrow function" syntax in the listen call.
+* This kind of terse syntax buys us little in an example like this,
+* so I comment it out and use the simpler but more verbose code
+* (even adding an actual callback function) for clarity.
+*/
+
+run_server ();
+
+// THE END
