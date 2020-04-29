f9346d0d5e5945ad89f7b35002127edbe8a8bc27
Tom Trebisky <tom@mmto.org>
2020-04-28 19:58:54 -0700

    Add timestamp code

--- node_3_temps/tserver.js
+++ node_3_temps/tserver.js
@@ -9,6 +9,42 @@ var Net = require('net');
 // const port = 2001;
 const port = 2002;
 
+// We want to generate this:
+//  04-28-2020 19:30:06
+
+function mk_timestamp ()
+{
+    var now = new Date();
+
+    hh = now.getHours(); 
+    mm = now.getMinutes();
+    ss = now.getSeconds();
+
+    mom = ("0" + (now.getMonth() + 1)).slice(-2);
+    dd = ("0" + now.getDate()).slice(-2);
+    y = now.getFullYear();
+
+    hms = hh + ":" + mm + ":" + ss;
+    mdy = mom + "-" + dd + "-" + y;
+
+    return mdy + " " + hms
+}
+
+/* You get either a buffer or a string,
+ * an ambiguity that I find annoying.
+ * so far my tests with telnet always yield a string
+ */
+function data_handler ( raw )
+{
+    var line;
+    // temps = chunk.toString();
+    // console.log ( 'Data received, ' + typeof(temps) );
+
+    temps = raw.toString().trim ();
+    line = mk_timestamp() + " " + temps
+    console.log ( 'Data: ' + line );
+}
+
 // There is also net.createServer()
 // I don't know why you would use that instead of this.
 var server = new Net.Server();
@@ -16,12 +52,6 @@ var server = new Net.Server();
 server.listen ( port, function() {
     console.log( `Listening on localhost:${port}` );
 });
-
-function data_handler ( chunk )
-{
-    temps = chunk.toString();
-    console.log ( 'Data: ' + temps );
-}
 
 /* When there is a connection, we only listen.
  */
@@ -41,5 +71,4 @@ server.on('connection', function(socket) {
         console.log(`Error: ${err}`);
     });
 });
-
 // THE END
